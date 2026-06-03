461753ad05da720693b8f45696d33aa2489b51e7
heddergottalbert-bit <heddergottalbert@gmail.com>
2026-06-03 13:26:04 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -481,6 +481,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         bronType: "verificatie",
         aiTekst: aiScore,
         claim: data.claim || "",
+        zoekterm: data.zoekterm || data.claim || "",
         bronBekend: false,
         onderwerpVerifieerbaar: false,
         verificatieBronnen: [],
@@ -615,3 +616,4 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
   }
 });
+
